2a4a6a9ea5797d09f32484fcd3fd041050299916
Sang Jae Park <48650641+SangJe@users.noreply.github.com>
2021-10-18 18:10:05 +0900

    Update Student.hh

--- lab8/Student.hh
+++ lab8/Student.hh
@@ -2,23 +2,11 @@
 #include <string>
 #include <vector>
 
-struct examInfo{
-    std::string grade;
-    int course_credits;
-};
-
 class Student{
 
 public:
     
 
 private:
-    // Note:Do not add any other member variables and you must initialize all these.
-    // Input
-    int stu_id, cur_seme, total_cour_credits;
-    char was_passed;
-    std::vector<examInfo> exam_vec;
 
-    // Output
-    std::string result;
 };
